3caab0d6280a5f7425e24c7d1f212535ba534c21
fred belotte <fredbelotte@outlook.com>
2019-11-21 17:08:23 -0600

    add mvc artifacts

---
+++ 2_mvc/pizzabox/PizzaBox.Client/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzaBox.Client.Models;
+
+namespace PizzaBox.Client.Controllers
+{
+  [Route("/[controller]/[action]")]
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet("{tyler}")]
+    public IActionResult Index(string tyler)
+    {
+      ViewBag.Name = tyler;
+      ViewData["Name"] = tyler;
+      TempData["Name"] = tyler;
+
+      return View(new Pizza());
+    }
+
+    [HttpPost()]
+    public IActionResult Order(Pizza p)
+    {
+      ViewBag.Pizza = p;
+      return View("Index", new Pizza());
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
