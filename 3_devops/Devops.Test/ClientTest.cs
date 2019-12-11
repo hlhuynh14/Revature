097b9a989011af31c76fdc3b6c0c1604d7176609
Henry <hlhuynh14@gmail.com>
2019-12-11 10:42:53 -0600

    add additional stages

--- 3_devops/Devops.Test/ClientTest.cs
+++ 3_devops/Devops.Test/ClientTest.cs
@@ -1,21 +1,36 @@
 using Devops.Client.Controllers;
+using Microsoft.Extensions.Logging;
 using Xunit;
-
-
 
 namespace Devops.Test
 {
-    public class ClientTest
+  public class ClientTest
+  {
+    private readonly ILogger<HomeController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<HomeController>();
+    [Fact]
+    public void Test_IndexPage()
     {
-        private readonly Illoger<HomeController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<HomeController>();
-        [Fact]
-        public void Test_IndexPage()
-        {
+      //arrange
+      var home = new HomeController(logger);
 
-            var home = new HomeController(logger);
-            var index = home.Index();
+      //act
+      var view = home.Index();
 
-            Assert.NotNull(index);
-        }
+      //assert
+      Assert.NotNull(view);
     }
+
+    [Fact]
+    public void Test_PrivacyPage()
+    {
+      //arrange
+      var home = new HomeController(logger);
+
+      //act
+      var view = home.Privacy();
+
+      //assert
+      Assert.NotNull(view);
+    }
+  }
 }
